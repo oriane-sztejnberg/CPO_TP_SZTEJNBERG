b0ada82db57d4fbcdc63ff1175e06141b61c7a2e
orian <orian@LAPTOP-MF6FOUD7>
2022-10-29 18:34:19 +0200

    création d'un nouveau projet versionné

--- TP2_manip_SZTEJNBERG/src/tp2_manip_sztejnberg/TP2_manip_SZTEJNBERG.java
+++ TP2_manip_SZTEJNBERG/src/tp2_manip_sztejnberg/TP2_manip_SZTEJNBERG.java
@@ -24,6 +24,40 @@ System.out.println("nb de calories de Assiette 1 : " +assiette1.nbCalories) ;
 assiette2.nbCalories+=100;
 System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
 System.out.println("nb de calories de Assiette 3 : " +assiette3.nbCalories) ;
+// inversement des assiettes 1 et 2:
+Tartiflette assietteinter=new Tartiflette (100);
+assietteinter=assiette1;
+assiette1=assiette2;
+assiette2=assietteinter;
+System.out.println("nb de calories de Assiette 1 : " +assiette1.nbCalories) ;
+System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
+//question6: une reference objet qui annonce referencer un type d'objet,
+//peut réferencer que ce type d'objet et pas un autre.
+
+Moussaka Moussakamere= new Moussaka(100);
+Moussaka Moussaka1=Moussakamere;
+Moussaka Moussaka2=Moussakamere;
+Moussaka Moussaka3=Moussakamere;
+Moussaka Moussaka4=Moussakamere;
+Moussaka Moussaka5=Moussakamere;
+Moussaka Moussaka6=Moussakamere;
+Moussaka Moussaka7=Moussakamere;
+Moussaka Moussaka8=Moussakamere;
+Moussaka Moussaka9=Moussakamere;
+Moussaka Moussaka10=Moussakamere;
+Moussaka1=new Moussaka(200);
+Moussaka2=new Moussaka(300);
+Moussaka3=new Moussaka(400);
+Moussaka4=new Moussaka(500);
+Moussaka5=new Moussaka(600);
+Moussaka6=new Moussaka(700);
+Moussaka7=new Moussaka(800);
+Moussaka8=new Moussaka(900);
+Moussaka9=new Moussaka(1000);
+Moussaka10=new Moussaka(1100);
+
+
+
     }
     
     
