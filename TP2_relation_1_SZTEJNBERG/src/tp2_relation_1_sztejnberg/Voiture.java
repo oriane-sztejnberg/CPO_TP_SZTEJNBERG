f7509b2bbe97f65edf10c4ea401ae1e48046b359
orian <orian@LAPTOP-MF6FOUD7>
2022-10-29 19:11:10 +0200

    création d'un nouveau projet versionné

---
+++ TP2_relation_1_SZTEJNBERG/src/tp2_relation_1_sztejnberg/Voiture.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_sztejnberg;
+
+/**
+ *
+ * @author orian
+ */
+public class Voiture {
+    String modele;
+    String marque;
+    int puissanceCV;
+    
+}
