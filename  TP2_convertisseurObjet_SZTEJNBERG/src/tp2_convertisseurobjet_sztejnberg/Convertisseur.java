87cb175b587136eb92eb5b9aeadf9b94520b6c32
orian <orian@LAPTOP-MF6FOUD7>
2022-10-29 20:26:40 +0200

    création d'un nouveau projet versionné

---  TP2_convertisseurObjet_SZTEJNBERG/src/tp2_convertisseurobjet_sztejnberg/Convertisseur.java
+++  TP2_convertisseurObjet_SZTEJNBERG/src/tp2_convertisseurobjet_sztejnberg/Convertisseur.java
@@ -13,12 +13,42 @@ public class Convertisseur {
 
 public Convertisseur(){
     nbconversion=0;
-
-public float CelciusVersKelvin (){
-    floatCelc= floatCelc+273,15;
-
-
-
 }
+
+public float CelciusVersKelvin (float floatCelc){
+    float floatKev= floatCelc +(float) (273.15);
+    nbconversion=nbconversion+1;
+    return floatKev;
+}
+public float KelvinVersCelcius (float floatKev){
+    float floatCelc= floatKev - (float) (273.15);
+    nbconversion=nbconversion+1;
+    return floatCelc;
+}
+public float FarenheitVersCelcius (float floatFar ){
+    float floatCelc= (floatFar - 32)/(float)(1.8);
+    nbconversion=nbconversion+1;
+    return floatCelc;
+}
+public float CelciustVersFarenheit (float floatCelc ){
+    float floatFar= (floatCelc *(float)(1.8))+32;
+    nbconversion=nbconversion+1;
+    return floatFar;
+}
+public float FarenheitVersKelvin (float floatFar ){
+    float floatKev= (floatFar - 32)/(float)(1.8)+(float)(273.15);
+    nbconversion=nbconversion+1;
+    return floatKev;
+}
+public float KelvinVersFarenheit (float floatKev ){
+    float floatFar= ((floatKev-(float)(273.15)) *(float)(1.8))+32;
+    nbconversion=nbconversion+1;
+    return floatFar;
+}
+@Override
+public String toString () {
+ return "nb de conversion = "+ nbconversion;
+}
+
 }
 
