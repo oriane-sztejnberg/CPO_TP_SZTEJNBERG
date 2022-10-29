1f2f6314c477fc1e5294605c88108f463e1c7bae
orian <orian@LAPTOP-MF6FOUD7>
2022-10-29 14:12:16 +0200

    création d'un nouveau projet versionné

--- TP2_Bieres_SZTEJNBERG/src/tp2_bieres_sztejnberg/TP2_Bieres_SZTEJNBERG.java
+++ TP2_Bieres_SZTEJNBERG/src/tp2_bieres_sztejnberg/TP2_Bieres_SZTEJNBERG.java
@@ -14,7 +14,12 @@ public class TP2_Bieres_SZTEJNBERG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       BouteilleBiere uneBiere = new BouteilleBiere() ;
+       uneBiere.nom = "Cuvée des trolls";
+       uneBiere.degreAlcool = 7.0 ;
+       uneBiere.brasserie="Dubuisson";
+       uneBiere.ouverte=false;
+       uneBiere.lireEtiquette();
     }
     
 }
